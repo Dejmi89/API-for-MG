3cc6381c7b2df07fa2f8ac38a9a8750040ca06ba
Dejmi89 <sticktothecode89@gmail.com>
2019-06-26 19:10:18 +0200

    ss

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('Server is ready to take messages');
+    console.log('Server is ready to take messsaages');
   }
 });
 
